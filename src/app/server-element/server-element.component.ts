75b784707d88186bde9b57b521be2f2b1a61ef52
Paul Santiago <paulsantiago282@gmail.com>
2023-07-28 20:32:15 -0400

    provide samples of lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentChecked, AfterContentInit,AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +6,16 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // if ViewEncapsulation.NONE anything added to this component's css will override the other components
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy
+ {
   // use @Input to make element property accessible outside server-element component you have the option to pass in an alias 'srvElement
   // NOTE: by default properites are only accessible to their individual component
   @Input('srvElement') //ability to have property be accessible for a parent component to use from the outside
@@ -15,9 +24,45 @@ export class ServerElementComponent implements OnInit {
     name: string,
     content: string,
   };
-  constructor() { }
+ @Input() name: string;
+  constructor() {
+    console.log('constructor called')
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ng-on-changes called')
+    console.log(changes)
 
+  }
   ngOnInit(): void {
+    console.log('ng-on-init-called')
+
   }
 
+ //gets called whenever there is a change called
+  ngDoCheck() {
+    console.log('ngdocheck called')
+  }
+  // only called once
+  ngAfterContentInit(){
+    console.log("ngAfterContent init called")
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContent Checked called")
+
+  }
+
+  ngAfterViewInit()  {
+    console.log("ngAfterView Init called")
+
+  }
+
+
+  ngAfterViewChecked(): void  {
+    console.log("ngAfterView Checked called")
+
+  }
+  ngOnDestroy(): void {
+     console.log("ngOnDestroy called")
+  }
 }
